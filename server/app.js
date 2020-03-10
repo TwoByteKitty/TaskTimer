bca0e9a4235ab31351fdcb7b7079d4528bca9161
Allison Renee Payne <allisonrenee131@hotmail.com>
2020-03-09 22:06:19 -0400

    Live reload and other fun stuff

--- server/app.js
+++ server/app.js
@@ -1,30 +1,38 @@
 const createError = require('http-errors');
+const debug = require('debug')('express-api:server');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const userRouter = require('./routes/userRoutes');
 const tasksRouter = require('./routes/taskRoutes');
-//If the tasks/* structure makes sense you dont need this.
-const createRouter = require('./routes/create');
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/taskTracker", { useNewUrlParser: true, useUnifiedTopology: true });
+const PORT = process.env.PORT || '3000';
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/taskTracker', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const app = express();
 
 // Register Handlebars view engine
-app.engine('.hbs', exphbs({
-  defaultLayout: 'main',
-  extname: '.hbs',
-  helpers: {
-    json: obj => JSON.stringify(obj),
-    equals: (val1, val2) => val1 === val2//This is a helper to see an object as a string on the page from handlebars.
-  }
-}));
+app.engine(
+  '.hbs',
+  exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    helpers: {
+      refresh: () => process.env.BROWSER_REFRESH_URL,
+      json: obj => JSON.stringify(obj),
+      equals: (val1, val2) => val1 === val2 //This is a helper to see an object as a string on the page from handlebars.
+    }
+  })
+);
 //Use handlebars view engine
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -39,21 +47,40 @@ app.use('/', indexRouter);
 app.use('/user', userRouter);
 app.use('/tasks', tasksRouter);
 
-
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { err });
 });
 
-module.exports = app;
+app.listen(PORT, err => {
+  if (err) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EADDRINUSE':
+        console.error(`Port: ${PORT} is already in use`);
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
+  debug(`Listening on port: ${PORT}`);
+  debug(`Environment: ${process.env.NODE_ENV}`);
+  if (process.env.NODE_ENV === 'development' && process.send) {
+    process.send({ event: 'online', url: `http://localhost:${PORT}/` });
+  }
+});
